6733a9d20cf395fca170bebec8f963fbb7df45a1
Blackman <DBlackman@int.co.chenango.ny.us>
2024-06-13 14:03:53 -0400

    Add axis and modify sizing

--- script.mjs
+++ script.mjs
@@ -5,29 +5,43 @@ d3.json(dataUrl)
     .then(data => {
 
         const cleanedData = data.data
+        const margins = {top: 20, right: 20, bottom: 20, left: 20}
+        const width = document.querySelector("body").clientWidth
+        const height = 150
 
         const svg = d3.select("#graph-wrapper")
             .append('svg')
-            .attr("width", 800)
-            .attr("height", 400);
+            .attr("viewbox", 0, 0, width, height)
+;
 
         const xScale = d3.scaleBand()
             .domain(cleanedData.map(d => d[0]))
-            .range([10, 790]);
+            .range([margins.left, width - margins.right]);
 
         const yScale = d3.scaleLinear()
             .domain([0, d3.max(cleanedData, d => d[1])])
-            .range([400, 0]);
+            .range([height - margins.bottom, margins.top]);
+
+        let xAxis = d3.axisBottom(xScale)
+        let yAxis = d3.axisLeft(yScale)
 
         svg.selectAll('rect')
             .data(cleanedData)
             .enter()
             .append('rect')
+            .attr('class', 'bar')
             .attr('x', d => xScale(d[0]))
             .attr('y', d => yScale(d[1]))
             .attr('width', xScale.bandwidth())
-            .attr('height', d => 400 - yScale(d[1]))
+            .attr('height', d => height - yScale(d[1]))
             .attr('fill', 'green')
+
+        svg.append("g")
+            .call(xAxis)
+
+        svg.append("g")
+            .call(yAxis)
+
     })    
     .catch(error => {
         console.error('Error fetching the data: ', error);
