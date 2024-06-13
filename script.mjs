5f47fe1fbb2876a956dbdcadc547c4b3643e07dc
Blackman <DBlackman@int.co.chenango.ny.us>
2024-06-13 13:32:07 -0400

    Bar graph construction

--- script.mjs
+++ script.mjs
@@ -1,19 +1,35 @@
 const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
-const width = "400px"
-const height = "300px"
 
-
-fetch(dataUrl).then(response => {
-    if (!response.ok) {
-        throw new Error("response not ok" + response.statusText)
-    }
-    return response.json()
-    })
+// fetch json
+d3.json(dataUrl)
     .then(data => {
-        
-        console.log(data)
-    })
+
+        const cleanedData = data.data
+
+        const svg = d3.select("#graph-wrapper")
+            .append('svg')
+            .attr("width", 800)
+            .attr("height", 400);
+
+        const xScale = d3.scaleBand()
+            .domain(cleanedData.map(d => d[0]))
+            .range([10, 790]);
+
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(cleanedData, d => d[1])])
+            .range([400, 0]);
+
+        svg.selectAll('rect')
+            .data(cleanedData)
+            .enter()
+            .append('rect')
+            .attr('x', d => xScale(d[0]))
+            .attr('y', d => yScale(d[1]))
+            .attr('width', xScale.bandwidth())
+            .attr('height', d => 400 - yScale(d[1]))
+            .attr('fill', 'green')
+    })    
     .catch(error => {
-        console.error("There has been an error", error)
-    })
+        console.error('Error fetching the data: ', error);
+    });
 
