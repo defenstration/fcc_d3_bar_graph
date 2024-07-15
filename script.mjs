e7c883977c34346a8e14ef0e8dbff1e09552dbdb
defenstration <david.blackman.career@gmail.com>
2024-07-15 17:44:35 -0400

    end of project

--- script.mjs
+++ script.mjs
@@ -1,84 +1,77 @@
-const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
 
-// fetch json
-d3.json(dataUrl)
-    .then(data => {
-        console.log(data)
-
-        const cleanedData = data.data
-        console.log(cleanedData.slice(0, 10))
-        const margins = {top: 20, right: 20, bottom: 20, left: 20}
-        const width = document.querySelector("#graph-wrapper").clientWidth
-        const height = 400
-
-        const svg = d3.select("#graph-wrapper")
-            .append('svg')
-            .attr("viewBox", [0, 0, width-margins.left - margins.right, height + margins.top + margins.bottom])
-;
-
-        const xScale = d3.scaleBand()
-            .domain(cleanedData.map(d => d[0]))
-            .range([0, width - margins.left -  margins.right]);
-
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(cleanedData, d => d[1])])
-            .range([height, 0]);
+d3.select('body')
+  .append('h1')
+  .text('D3 Bar Chart Project')
+  .attr('id', 'title')
 
 
-        let xAxis = d3.axisBottom(xScale)
-        let yAxis = d3.axisLeft(yScale)
-
-        xAxis.ticks(20)
-
-        yAxis.ticks(10)
-
-        svg.selectAll('rect')
-            .data(cleanedData)
-            .enter()
-            .append('rect')
-            .attr('class', 'bar')
-            .attr('x', d => xScale(d[0]))
-            .attr('y', d => yScale(d[1]))
-            .attr('width', xScale.bandwidth())
-            .attr('height', d => height - yScale(d[1]))
-            .attr("data-date", (d)=>d[0])
-            .attr("data-gdp", (d)=>d[1])
-            .append("title")
-            .text((d) => d)
-            .attr("id", "tooltip")
-            .attr("data-date", (d)=>d[0])
-
-        svg.append("g")
-            .attr("id", "x-axis")
-            .attr("transform", `translate(0,${height})`)
-            .call(xAxis)
-
-        svg.append("g")
-            .attr("id", "y-axis")
-            .call(yAxis)
-
-        svg.append('text')
-            .attr("class", "axis-label")
-            .attr('x', width / 2)
-            .attr('y', height + 40)
-            .text("X Axis Label")
-
-        svg.append('text')
-            .attr("class", "axis-label")
-            .attr("text-anchor", "end")
-            .attr("transform", "rotate(-90)")
-            .attr('x', -height / 2)
-            .attr('y', 10)
-            .text("Y Axis Label")
-
-        svg.append('text')
-            .attr("id", "title")
-            .attr("x", width / 2)
-            .attr("y", 5 + (margins.top /2))
-            .text("Chart Title")
-
-    })    
-    .catch(error => {
-        console.error('Error fetching the data: ', error);
-    });
-
+d3.json(url)
+  .then(data => {
+  
+  let h = 500
+  let w = 1000
+  
+  const svg = d3.select('body')
+              .append('svg')
+              .attr('height', h + 40)
+              .attr('width', w + 40)
+  
+  let graphValues = data['data']
+  
+  let parseDate = d3.timeParse(`%Y-%m-%d`)
+  let formatDate = d3.timeFormat(`%Y-%m-%d`)
+  
+  let parsedValues = graphValues.map(d => [parseDate(d[0]), d[1]])
+      
+  const barWidth = w / parsedValues.length
+  
+  let x = d3.scaleTime()
+            .domain(d3.extent(parsedValues, d => d[0]))
+            .range([0, w])
+  
+  let y = d3.scaleLinear()
+            .domain([0, d3.max(parsedValues, d => d[1])])
+            .range([h, 0])
+  
+  svg.append('g')
+     .attr('id', 'x-axis')
+     .call(d3.axisBottom(x)
+             .ticks(d3.timeYear.every(5))
+             .tickFormat(d3.timeFormat('%Y')))
+     .attr('transform', `translate(0, ${h})`)
+     .selectAll('text')
+     .attr('transform', 'rotate(-45)')
+     .selectAll('ticks')
+     .attr('class', 'tick')
+  
+  svg.append('g')
+     .attr('id', 'y-axis')
+     .call(d3.axisLeft(y))
+  
+  const tooltip = d3.select('#tooltip')
+  
+  svg.selectAll('rect')
+     .data(parsedValues)
+     .enter()
+     .append('rect')
+     .attr('class', 'bar')
+     .attr('x', d => x(d[0]))
+     .attr('y', d => y(d[1]))
+     .attr("height", d => h - y(d[1]))
+     .attr('width', barWidth)
+     .attr('data-date', d => formatDate(d[0]))
+     .attr('data-gdp', d => d[1])
+     .on('mouseover', (event, d) => {
+        tooltip.transition().style('opacity', .9)
+        tooltip.style('left', (event.pageX + 10) + 'px')
+               .style('top', (event.pageY + 10) + 'px')
+               .attr('data-date', `${formatDate(d[0])}`)
+               .html(`Year: ${d3.timeFormat('%Y-%m-%d')(d[0])}. GDP: ${d[1]}`);             
+               })
+      .on('mouseout', (event) => {
+        tooltip.style('opacity', 0);
+        })
+     .append('title')
+     .text(d =>`${d[0]}, ${d[1]}`);
+})
